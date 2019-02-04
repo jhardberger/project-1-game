fa99846fbf97c7caaa741cef97d0f885dfb8736d
John <john.hardberger@gmail.com>
2019-02-04 11:34:05 -0600

    updating images

--- js/app.js
+++ js/app.js
@@ -12,18 +12,18 @@ const ctx = canvas.getContext('2d');
 
 //ART*******************************************************************************
 
-// const gifFrames = [
-// 	"/Users/john/salty-sardines/project-1-game/new_game_images/swing1.gif",
-// 	"/Users/john/salty-sardines/project-1-game/new_game_images/swing2.gif",
-// 	"/Users/john/salty-sardines/project-1-game/new_game_images/swing3.gif",
-// 	"/Users/john/salty-sardines/project-1-game/new_game_images/swing4.gif",
-// 	"/Users/john/salty-sardines/project-1-game/new_game_images/swing5.gif",
-// 	"/Users/john/salty-sardines/project-1-game/new_game_images/swing6.gif",
-// 	"/Users/john/salty-sardines/project-1-game/new_game_images/swing7.gif",
-// 	"/Users/john/salty-sardines/project-1-game/new_game_images/swing8.gif",
-// 	"/Users/john/salty-sardines/project-1-game/new_game_images/swing9.gif",
-// 	"/Users/john/salty-sardines/project-1-game/new_game_images/swing10.gif",
-// ];
+const gifFrames = [
+	"/Users/john/salty-sardines/project-1-game/new_game_images/swing1.gif",
+	"/Users/john/salty-sardines/project-1-game/new_game_images/swing2.gif",
+	"/Users/john/salty-sardines/project-1-game/new_game_images/swing3.gif",
+	"/Users/john/salty-sardines/project-1-game/new_game_images/swing4.gif",
+	"/Users/john/salty-sardines/project-1-game/new_game_images/swing5.gif",
+	"/Users/john/salty-sardines/project-1-game/new_game_images/swing6.gif",
+	"/Users/john/salty-sardines/project-1-game/new_game_images/swing7.gif",
+	"/Users/john/salty-sardines/project-1-game/new_game_images/swing8.gif",
+	"/Users/john/salty-sardines/project-1-game/new_game_images/swing9.gif",
+	"/Users/john/salty-sardines/project-1-game/new_game_images/swing10.gif",
+];
 
 //CLASSES***************************************************************************
 
@@ -241,32 +241,19 @@ const metaGame = {
 		$('#loadingImg').attr('src', 'https://imgur.com/973tgMm')
 
 		gifFrames = [
-			"/Users/john/salty-sardines/project-1-game/new_game_images/swing1.gif",
-			"/Users/john/salty-sardines/project-1-game/new_game_images/swing2.gif",
-			"/Users/john/salty-sardines/project-1-game/new_game_images/swing3.gif",
-			"/Users/john/salty-sardines/project-1-game/new_game_images/swing4.gif",
-			"/Users/john/salty-sardines/project-1-game/new_game_images/swing5.gif",
-			"/Users/john/salty-sardines/project-1-game/new_game_images/swing6.gif",
-			"/Users/john/salty-sardines/project-1-game/new_game_images/swing7.gif",
-			"/Users/john/salty-sardines/project-1-game/new_game_images/swing8.gif",
-			"/Users/john/salty-sardines/project-1-game/new_game_images/swing9.gif",
-			"/Users/john/salty-sardines/project-1-game/new_game_images/swing10.gif",
+
+			'https://imgur.com/9PEjarE',
+			'https://imgur.com/V9yoihz',
+			'https://imgur.com/7sFw49R',
+			'https://imgur.com/n1I450t',
+			'https://imgur.com/tqzaG60',
+			'https://imgur.com/THAFaJq',
+			'https://imgur.com/jvOTqbz',
+			'https://imgur.com/zAs2CoS',
+			'https://imgur.com/zng342B',
+			'https://imgur.com/uIgsUDk'
+
 		];
-
-		// [
-
-		// 	'https://imgur.com/9PEjarE',
-		// 	'https://imgur.com/V9yoihz',
-		// 	'https://imgur.com/7sFw49R',
-		// 	'https://imgur.com/n1I450t',
-		// 	'https://imgur.com/tqzaG60',
-		// 	'https://imgur.com/THAFaJq',
-		// 	'https://imgur.com/jvOTqbz',
-		// 	'https://imgur.com/zAs2CoS',
-		// 	'https://imgur.com/zng342B',
-		// 	'https://imgur.com/uIgsUDk'
-
-		// ];
 
 		
 	}
